22373e03975953a3d6413a1714989014aad15ef9
mughaldayaan <dayaanmughal@gmail.com>
2025-12-27 14:05:06 +0500

    Describe your changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,10 +29,10 @@ export default function Navbar(props) {
         <button className="btn btn-primary" type="submit">Search</button>
       </form>
       <div className="d-flex">
-        <div className="bg-primary rounded mx-2 my-1" onClick={()=>props.toggleMode('primary')} style ={{height:"30px" ,width:"30px",cursor:'pointer'}}></div>
-        <div className="bg-success rounded mx-2 my-1" onClick={()=>props.toggleMode('success')} style ={{height:"30px" ,width:"30px",cursor:'pointer'}}></div>
-        <div className="bg-warning rounded mx-2 my-1" onClick={()=>props.toggleMode('warning')} style ={{height:"30px" ,width:"30px",cursor:'pointer'}}></div>
-        <div className="bg-danger rounded mx-2 my-1" onClick={()=>props.toggleMode('danger')} style ={{height:"30px" ,width:"30px",cursor:'pointer'}}></div>
+        <div className="bg-primary rounded mx-2 my-2" onClick={()=>props.toggleMode('primary')} style ={{height:"30px" ,width:"30px",cursor:'pointer'}}></div>
+        <div className="bg-success rounded mx-2 my-2" onClick={()=>props.toggleMode('success')} style ={{height:"30px" ,width:"30px",cursor:'pointer'}}></div>
+        <div className="bg-warning rounded mx-2 my-2" onClick={()=>props.toggleMode('warning')} style ={{height:"30px" ,width:"30px",cursor:'pointer'}}></div>
+        <div className="bg-danger rounded mx-2 my-2" onClick={()=>props.toggleMode('danger')} style ={{height:"30px" ,width:"30px",cursor:'pointer'}}></div>
 
       </div>
       <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'} ms-3`}>
